3312815337531d35ff9dc6ca455ca2d4ff7603b3
sanehakaur77 <snehaswali9988@gmail.com>
2025-07-22 21:38:20 +0530

    Update db.js

--- Day14/backend/configs/db.js
+++ Day14/backend/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose');
-const connection=mongoose.connect('mongodb+srv://Saneha9988:2JrsYkANQcbWf6BV@cluster0.lp0seqg.mongodb.net/saneha?retryWrites=true&w=majority&appName=Cluster0');
+const connection=mongoose.connect('string');
 module.exports={
     connection
 }
